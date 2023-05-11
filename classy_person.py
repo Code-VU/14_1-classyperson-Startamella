aee5ce357f9a253e8de958d1a10fb85d6f539672
Samantha Tartamella <Samantha.Tartamella@vu.com>
2023-05-11 15:29:53 -0500

    person

--- classy_person.py
+++ classy_person.py
@@ -9,7 +9,23 @@ don't forget the use of 'self' and to have the methods:
 
 class Person:
 
+    def __init__(self, age, name) : 
+        self.name = name
+        self.age = age 
+    
+    def increase_age(self) : 
+        self.age = self.age + 1 
+        print(self.age)
 
+    def say_greeting(self) : 
+        print("Hello world! My name is " + self.name + "!")
+
+    def count_to_age(self) :
+        count = 0 
+        while count < self.age : 
+            print(count)
+            count = count + 1 
+        
 
 
 # You won't need to call anything here.
